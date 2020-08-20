99b0deb44ab2a7db1b001d0bcb54f8f92e93ccd5
Gleb Korzan <expc@glowwwork.com>
2020-08-20 22:09:14 +0300

    tutorial p2

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,5 +1,8 @@
 extends KinematicBody2D
 
+const ACCELERATION = 10
+const MAX_SPEED = 100
+const FRICTION = 10
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
@@ -8,8 +11,9 @@ func _physics_process(delta):
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector
+		velocity += input_vector.normalized() * ACCELERATION * delta
+		velocity = velocity.clamped(MAX_SPEED * delta)
 	else:
-		velocity = Vector2.ZERO
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	move_and_collide(velocity)
 	
